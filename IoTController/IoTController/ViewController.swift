1e0cbc7ba822ed8b9825bc4d058ab2d403d34a6e
Willie Shen <williesh@usc.edu>
2020-04-22 15:25:38 -0700

    Adding the UI

---
+++ IoTController/IoTController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IoTController
+//
+//  Created by Willie Shen on 4/22/20.
+//  Copyright © 2020 Willie Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
