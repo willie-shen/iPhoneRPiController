a9fc0a96565de59ce48294f7a36ea8c4d74c2f17
Willie Shen <williesh@usc.edu>
2020-05-04 19:05:46 -0700

    Changing host

--- IoTController/IoTController/ViewController.swift
+++ IoTController/IoTController/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 import CocoaMQTT
 import CocoaAsyncSocket
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CocoaMQTTDelegate {
+
+    
 
     @IBOutlet weak var brightness: UILabel!
     
@@ -33,13 +35,20 @@ class ViewController: UIViewController {
         //https://www.hackingwithswift.com/example-code/language/how-to-convert-a-float-to-an-int
         
        //client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-        mqtt = CocoaMQTT(clientID: "123", host: "eclipse.usc.edu", port: 11000)
+        //host="104.33.88.107", port=1883
+        mqtt = CocoaMQTT(clientID: "123", host: "54.197.16.207", port: 1883)
+        mqtt.delegate = self
+       
+        /*mqtt.didConnectAck = {CocoaMQTT, CocoaMQTTConnAck in
+                 print("Connected")
+             }*/
         
         // ...
-        mqtt.subscribe("buttonpress")
-        mqtt.subscribe("dimUpdate")
         
-        mqtt.didReceiveMessage = { mqtt, message, id in
+        
+     
+        
+        /*mqtt.didReceiveMessage = { mqtt, message, id in
             
             print(message.topic)
             if(message.topic == "dimUpdate"){
@@ -59,7 +68,7 @@ class ViewController: UIViewController {
             }
             
             
-        }
+        } */
         _ = mqtt.connect()
         
     }
@@ -71,6 +80,59 @@ class ViewController: UIViewController {
         var voltage:Double = Double(brightness * 5)/1023.0
         
         mqtt.publish("buttonpress", withString: "\(brightness)" )
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        mqtt.subscribe("buttonpress")
+        mqtt.subscribe("dimUpdate")
+        print("Connected")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
+        print(message.topic)
+                   if(message.topic == "dimUpdate"){
+                       
+                       //https://www.hackingwithswift.com/example-code/language/how-to-convert-data-to-a-string
+                       var voltage:Int = Int(String(decoding:message.payload, as:UTF8.self)) ?? 0
+                       
+                       var brightnessLevel:Float = (Float(voltage)/1023) * 100
+                       self.adjuster.value = brightnessLevel
+                       
+                       self.brightness.text = "\(Int(self.adjuster.value))"
+                       
+                       print("received")
+                       
+                       
+                       
+                   }
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topics: [String]) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        
     }
     
 }
